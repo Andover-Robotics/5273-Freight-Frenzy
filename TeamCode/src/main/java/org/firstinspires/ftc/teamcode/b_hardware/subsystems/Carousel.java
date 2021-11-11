7b1ad82e936e4287a3283aa861776626b707e670
Brozone0 <alexyou2006@outlook.com>
2021-11-10 23:24:15 -0500

    changed code with alliance switching in mind more forward compatibility

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/b_hardware/subsystems/Carousel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/b_hardware/subsystems/Carousel.java
@@ -11,21 +11,45 @@ import org.firstinspires.ftc.teamcode.GlobalConfig;
 
 @Config
 public class Carousel extends SubsystemBase {
-    public static final double SPEED = 1;
+    public static final double MAX_SPEED = (int) 1150 / 60 * 145.1;
+    public static final double SPEED_PERCENT = 1150 / 750;
+    public double targetSpeed = 0.0;
     private MotorEx motor;
+
+    private enum state {
+        ON,
+        OFF
+    }
+    private state runState = state.OFF;
+
 
     public Carousel(OpMode opMode){
         motor = new MotorEx(opMode.hardwareMap, "carousel", Motor.GoBILDA.RPM_1150);
-        motor.setRunMode(Motor.RunMode.RawPower);
+        motor.setRunMode(Motor.RunMode.VelocityControl);
         motor.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
         motor.motor.setDirection(GlobalConfig.alliance == GlobalConfig.Alliance.RED ? DcMotorSimple.Direction.REVERSE : DcMotorSimple.Direction.FORWARD);
     }
 
-    public void run(){
-        motor.set(SPEED);
+    public void runToggle() {
+        if(runState == state.OFF) {
+            run();
+        }
+        else if(runState == state.ON) {
+            stop();
+        }
     }
 
-    public void stop(){
-        motor.set(0);
+    public void run() {
+        motor.motorEx.setVelocity(MAX_SPEED * SPEED_PERCENT);
+        runState = state.ON;
     }
+
+    public void stop() {
+        motor.motorEx.setVelocity(0.0);
+        runState = state.OFF;
+    }
+
+
+
+
 }
