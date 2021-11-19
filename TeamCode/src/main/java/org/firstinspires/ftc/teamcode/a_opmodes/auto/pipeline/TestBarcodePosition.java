8595f4c410b203cd8e8f84ed4a629147e556ba92
Brozone0 <alexyou2006@outlook.com>
2021-11-19 16:52:29 -0500

    added a new test opmode for openCV (specifically barcode detection)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/a_opmodes/auto/pipeline/TestBarcodePosition.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.a_opmodes.auto.pipeline;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class TestBarcodePosition extends LinearOpMode {
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
