3883ecd2a74150d97d1c41792483a4d18b0b9890
Brozone0 <alexyou2006@outlook.com>
2021-11-29 23:51:15 -0500

    adding cube weight detection (incomplete) to the ControlHub.java and fixing partyMode
    Signed-off-by: Brozone0 <alexyou2006@outlook.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/b_hardware/subsystems/ControlHub.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/b_hardware/subsystems/ControlHub.java
@@ -1,10 +1,16 @@
 package org.firstinspires.ftc.teamcode.b_hardware.subsystems;
 
 import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.arcrobotics.ftclib.hardware.motors.MotorEx;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
+import org.checkerframework.checker.units.qual.A;
 import org.openftc.revextensions2.ExpansionHubEx;
 import org.openftc.revextensions2.ExpansionHubMotor;
+
+import java.util.ArrayList;
 
 class Hubs extends SubsystemBase {
 
@@ -13,12 +19,20 @@ class Hubs extends SubsystemBase {
 
 
 
+    // variables for current draw logic
+    private static double leftAvgMilliamps;
+    private static double rightAvgMilliamps;
+    // TODO: tune milliamp draw for different weight cubes --> put in constants RELATIVE to the avg amps
+    private static double LIGHT_WEIGHT_CUBE_MILLIAMP_DRAW = 0.0, MEDIUM_WEIGHT_CUBE_MILLIAMP_DRAW = 0.0, HEAVY_WEIGHT_CUBE_MILLIAMP_DRAW;
+
+
     private static boolean partyMode = false;
     private static int[] rgb = {0, 0, 0};
 
     public Hubs(OpMode opMode) {
         controlHub = opMode.hardwareMap.get(ExpansionHubEx.class, "Control Hub");
         expansionHub = opMode.hardwareMap.get(ExpansionHubEx.class, "Expansion Hub 2");
+        calibrateIntakeAvgAmperage(opMode);
     }
 
 
@@ -67,6 +81,39 @@ class Hubs extends SubsystemBase {
     }
     public double rightIntakeCurrentDraw() {
         return controlHub.getMotorCurrentDraw(ExpansionHubEx.CurrentDrawUnits.MILLIAMPS, 2);
+    }
+
+// TODO: possible calipration thing durinig init to run the motor and get the current it currently draws and average
+//    for more accurate measurements during auto & teleOp also find a way to display it on the driver hub or something
+//    BUT -- in the future use the data from the lsat 5 seconds to keep a floating average for better accuracy in cube weight detection
+
+    public void calibrateIntakeAvgAmperage(OpMode opMode) {
+
+        MotorEx leftIntake = new MotorEx(opMode.hardwareMap, "leftIntake", Motor.GoBILDA.RPM_312);
+        leftIntake.setRunMode(Motor.RunMode.RawPower);
+        leftIntake.motor.setDirection(DcMotorSimple.Direction.REVERSE);
+        MotorEx rightIntake = new MotorEx(opMode.hardwareMap, "rightIntake", Motor.GoBILDA.RPM_312);
+        rightIntake.setRunMode(Motor.RunMode.RawPower);
+        rightIntake.motor.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        final double CALIBRATION_VELOCITY = 1.0;
+
+        ArrayList<Double> leftList = new ArrayList<>();
+        ArrayList<Double> rightList = new ArrayList<>();
+
+        leftIntake.set(CALIBRATION_VELOCITY);
+        rightIntake.set(CALIBRATION_VELOCITY);
+        
+        // running the intakes and putting their averages in an arraylist to then get the average;
+
+        for(int i = 0; i < 50000; i++) {
+            if(i % 50 == 0) {
+                leftList.add(leftIntakeCurrentDraw());
+                rightList.add(rightIntakeCurrentDraw());
+            }
+        }
+
+
     }
 
 
