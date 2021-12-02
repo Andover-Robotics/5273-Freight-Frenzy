4c31b5615a6f4b9a602cc7ef50b5a5fbe955dec9
Brozone0 <alexyou2006@outlook.com>
2021-12-02 01:12:45 -0500

    added tasks
    Signed-off-by: Brozone0 <alexyou2006@outlook.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/a_opmodes/teleop/IntakeTuneOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/a_opmodes/teleop/IntakeTuneOp.java
@@ -2,6 +2,12 @@ package org.firstinspires.ftc.teamcode.a_opmodes.teleop;
 
 public class IntakeTuneOp extends BaseOpMode{
 
+    // TODO:
+    //      Make this tuning opMode so that it can Change the speed of the intake &
+    //      display the amperage draw values in a Dashboard to test the
+    //      viability of using amperage draw to detect cube weight
+
+
     @Override
     void subInit() {
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/a_opmodes/teleop/SlideTuneOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/a_opmodes/teleop/SlideTuneOp.java
@@ -1,6 +1,11 @@
 package org.firstinspires.ftc.teamcode.a_opmodes.teleop;
 
 public class SlideTuneOp extends BaseOpMode{
+
+    //TODO:
+    //     Make a tuning opMode to get the tolerance of the slides as low as possible
+    //     To do this maybe make the motor run to a position and be able to change the tolerance during the opMode
+
     @Override
     void subInit() {
 
