1d02ef5fc796eec1b513a9024229550cc03c952b
Brozone0 <alexyou2006@outlook.com>
2022-01-03 12:11:50 -0500

    Make field centric rely on two imus
    Becuase of the drift that the robot currently has in field centric,
    having the second imu average out the first one,
    while still being able to reset their respective offsets
    may make field centric movement more reliable
    
    Signed-off-by: Brozone0 <alexyou2006@outlook.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/a_opmodes/teleop/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/a_opmodes/teleop/MainTeleOp.java
@@ -1,12 +1,14 @@
 package org.firstinspires.ftc.teamcode.a_opmodes.teleop;
 
 import com.arcrobotics.ftclib.command.CommandScheduler;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys.Button;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys.Trigger;
 import com.arcrobotics.ftclib.geometry.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.teamcode.c_drive.RRMecanumDrive;
 import org.firstinspires.ftc.teamcode.c_drive.RRMecanumDrive.Mode;
 import org.firstinspires.ftc.teamcode.d_util.utilclasses.TimingScheduler;
 
@@ -17,7 +19,9 @@ public class MainTeleOp extends BaseOpMode {//required vars here
   private TimingScheduler timingScheduler;
   private boolean centricity = false;
   private final double TRIGGER_CONSTANT = 0.15;
-  private final double SLOW_MODE_SPEED = 0.4;
+  private final double SLOW_MODE_PERCENT = 0.4;
+  private double fieldCentricOffset0 = 0.0;
+  private double fieldCentricOffset1 = 0.0;
 
   //config? stuff here =========================================================================
 
@@ -26,6 +30,7 @@ public class MainTeleOp extends BaseOpMode {//required vars here
   //opmode vars here ==============================================================================================
   public void subInit() {
     timingScheduler = new TimingScheduler(this);
+    driveSpeed = 1.0;
   }
 
   @Override
@@ -142,54 +147,67 @@ public class MainTeleOp extends BaseOpMode {//required vars here
     telemetry.addData("x", bot.roadRunner.getPoseEstimate().getX());
     telemetry.addData("y", bot.roadRunner.getPoseEstimate().getY());
     telemetry.addData("heading", bot.roadRunner.getPoseEstimate().getHeading());
-    telemetry.addData("current raw angle", bot.imu0.getAngularOrientation().toAngleUnit(AngleUnit.DEGREES).firstAngle);
+    telemetry.addData("driver left stick", "left X" + gamepadEx1.getLeftX() + ": " + gamepadEx1.getLeftY());
+
   }
 
 
   private void drive(){//Driving ===================================================================================
-    final double gyroAngle =
+    final double gyroAngle0 =
             bot.imu0.getAngularOrientation().toAngleUnit(AngleUnit.DEGREES).firstAngle
-                    - fieldCentricOffset;
+                    - fieldCentricOffset0;
+    final double gyroAngle1 = (bot.imu1 != null) ?
+            bot.imu1.getAngularOrientation().toAngleUnit(AngleUnit.DEGREES).firstAngle
+                    - fieldCentricOffset1
+            : gyroAngle0;
+    final double avgGyroAngle = (gyroAngle0 + gyroAngle1)/2;
+
     Vector2d driveVector = new Vector2d(gamepadEx1.getLeftX(), gamepadEx1.getLeftY()),
             turnVector = new Vector2d(
                     gamepadEx1.getRightX() * Math.abs(gamepadEx1.getRightX()),
                     0);
-    if (bot.roadRunner.mode == Mode.IDLE) {
+    if (bot.roadRunner.mode == RRMecanumDrive.Mode.IDLE) {
 
-      boolean dpadPressed = (gamepadEx1.getButton(Button.DPAD_DOWN) || gamepadEx1.getButton(Button.DPAD_UP)
-              || gamepadEx1.getButton(Button.DPAD_LEFT) || gamepadEx1.getButton(Button.DPAD_RIGHT));
-      boolean buttonPressed = (gamepadEx1.getButton(Button.X) || gamepadEx1.getButton(Button.B));
-      double forwardSpeed = (gamepadEx1.getButton(Button.DPAD_LEFT) || gamepadEx1.getButton(Button.DPAD_RIGHT)) ? (gamepadEx1.getButton(Button.DPAD_RIGHT) ? 1 : -1) : 0;
-      double strafeSpeed = (gamepadEx1.getButton(Button.DPAD_DOWN) || gamepadEx1.getButton(Button.DPAD_UP)) ? (gamepadEx1.getButton(Button.DPAD_UP) ? 1 : -1) : 0;
-      double turnSpeed = (gamepadEx1.getButton(Button.X) || gamepadEx1.getButton(Button.B)) ? (gamepadEx1.getButton(Button.B) ? 1 : -1) : 0;
+      boolean dpadPressed = (gamepadEx1.getButton(GamepadKeys.Button.DPAD_DOWN) || gamepadEx1.getButton(GamepadKeys.Button.DPAD_UP)
+              || gamepadEx1.getButton(GamepadKeys.Button.DPAD_LEFT) || gamepadEx1.getButton(GamepadKeys.Button.DPAD_RIGHT));
+      boolean buttonPressed = (gamepadEx1.getButton(GamepadKeys.Button.X) || gamepadEx1.getButton(GamepadKeys.Button.B));
 
-      if (centricity) //epic java syntax
+      double forwardSpeed = (gamepadEx1.getButton(GamepadKeys.Button.DPAD_LEFT) || gamepadEx1.getButton(GamepadKeys.Button.DPAD_RIGHT)) ? (gamepadEx1.getButton(GamepadKeys.Button.DPAD_RIGHT) ? 1 : -1) : 0;
+      double strafeSpeed = (gamepadEx1.getButton(GamepadKeys.Button.DPAD_DOWN) || gamepadEx1.getButton(GamepadKeys.Button.DPAD_UP)) ? (gamepadEx1.getButton(GamepadKeys.Button.DPAD_UP) ? 1 : -1) : 0;
+      double turnSpeed = (gamepadEx1.getButton(GamepadKeys.Button.X) || gamepadEx1.getButton(GamepadKeys.Button.B)) ? (gamepadEx1.getButton(GamepadKeys.Button.B) ? 1 : -1) : 0;
+
+      if (centricity) {//epic java syntax
         bot.drive.driveFieldCentric(
                 driveVector.getY() * driveSpeed,
                 driveVector.getX() * -driveSpeed,
                 turnVector.getX() * driveSpeed,
-                gyroAngle);
-
-      else if (dpadPressed || buttonPressed)
+                avgGyroAngle);
+      }
+      else if (dpadPressed || buttonPressed) {
+        driveSpeed *= SLOW_MODE_PERCENT;
         bot.drive.driveRobotCentric(
-                strafeSpeed * SLOW_MODE_SPEED,
-                forwardSpeed * -SLOW_MODE_SPEED,
-                turnSpeed * SLOW_MODE_SPEED
-          );
+                strafeSpeed * SLOW_MODE_PERCENT * driveSpeed,
+                forwardSpeed * -SLOW_MODE_PERCENT * driveSpeed,
+                turnSpeed * SLOW_MODE_PERCENT * driveSpeed
+        );
+      }
 
-      else
+      else {
         bot.drive.driveRobotCentric(
                 driveVector.getY() * driveSpeed,
                 driveVector.getX() * -driveSpeed,
                 turnVector.getX() * driveSpeed
         );
+      }
 
     }
-    if (gamepadEx1.wasJustPressed(Button.LEFT_STICK_BUTTON)) {
-      fieldCentricOffset = bot.imu0.getAngularOrientation()
-          .toAngleUnit(AngleUnit.DEGREES).firstAngle;
+    if (gamepadEx1.wasJustPressed(GamepadKeys.Button.LEFT_STICK_BUTTON)) {
+      fieldCentricOffset0 = bot.imu0.getAngularOrientation()
+              .toAngleUnit(AngleUnit.DEGREES).firstAngle;
+      fieldCentricOffset1 = bot.imu1.getAngularOrientation()
+              .toAngleUnit(AngleUnit.DEGREES).firstAngle;
     }
-    if (gamepadEx1.wasJustPressed(Button.RIGHT_STICK_BUTTON)){
+    if (gamepadEx1.wasJustPressed(GamepadKeys.Button.RIGHT_STICK_BUTTON)){
       centricity = !centricity;
     }
 
