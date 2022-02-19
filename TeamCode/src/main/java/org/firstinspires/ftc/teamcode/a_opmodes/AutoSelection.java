091fff3add2db63558400f3d4c2dc9152086b7af
aadarsh-arasu <84949353+aadarsh-arasu@users.noreply.github.com>
2022-02-19 01:13:14 -0500

    clean auto selecting opmodes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/a_opmodes/AutoSelection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/a_opmodes/AutoSelection.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.a_opmodes;
 
+import android.opengl.GLES10;
 import android.provider.Settings;
 
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
@@ -10,10 +11,10 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.teamcode.GlobalConfig;
 import org.firstinspires.ftc.teamcode.a_opmodes.teleop.BaseOpMode;
 
-@Autonomous(name="Autonomous Selection", group = "Competition")
+@Autonomous(name = "Autonomous Selection", group = "Competition")
 
 public class AutoSelection extends BaseOpMode {
-    //todo combine this opmode with alliance selection and side selection
+    //todo clean up opmodes, so the selection screen is not cluttered
     @Override
     protected void subInit() {
         telemetry.addData("Controls", "X: carousel, Y:outtakeCube, B:parking, D-Pad: cycles");
@@ -26,17 +27,37 @@ public class AutoSelection extends BaseOpMode {
         telemetry.addData("OuttakeCube", GlobalConfig.outtakeCube);
         telemetry.addData("Cycles", GlobalConfig.cycles);
         telemetry.addData("Parking", GlobalConfig.parking);
+        telemetry.addData("Allaince", GlobalConfig.alliance);
+        telemetry.addData("Side", GlobalConfig.side);
         telemetry.update();
 
         if (gamepadEx1.wasJustReleased(GamepadKeys.Button.B))
             GlobalConfig.carousel = !GlobalConfig.carousel;
         else if (gamepadEx1.wasJustReleased(GamepadKeys.Button.Y))
             GlobalConfig.outtakeCube = !GlobalConfig.outtakeCube;
-        else if (gamepadEx1.wasJustReleased(GamepadKeys.Button.B))
+        else if (gamepadEx1.wasJustReleased(GamepadKeys.Button.X))
             GlobalConfig.parking = !GlobalConfig.parking;
         else if (gamepadEx1.wasJustReleased(GamepadKeys.Button.DPAD_UP))
             GlobalConfig.cycles += 1;
         else if (gamepadEx1.wasJustReleased(GamepadKeys.Button.DPAD_DOWN))
             GlobalConfig.cycles -= 1;
+        else if (gamepadEx1.wasJustReleased(GamepadKeys.Button.A)) {
+            if (GlobalConfig.alliance == GlobalConfig.Alliance.BLUE)
+                GlobalConfig.alliance = GlobalConfig.Alliance.RED;
+            else if (GlobalConfig.alliance == GlobalConfig.Alliance.RED)
+                GlobalConfig.alliance = GlobalConfig.Alliance.BLUE;
+        }
+        else if (gamepadEx1.wasJustReleased(GamepadKeys.Button.LEFT_BUMPER)){
+            if (GlobalConfig.alliance == GlobalConfig.Alliance.RED)
+                GlobalConfig.side = GlobalConfig.Side.DEPOT;
+            else if (GlobalConfig.alliance == GlobalConfig.Alliance.BLUE)
+                GlobalConfig.side = GlobalConfig.Side.WAREHOUSE;
+        }
+        else if (gamepadEx1.wasJustReleased(GamepadKeys.Button.RIGHT_BUMPER)){
+            if (GlobalConfig.alliance == GlobalConfig.Alliance.RED)
+                GlobalConfig.side = GlobalConfig.Side.WAREHOUSE;
+            else if (GlobalConfig.alliance == GlobalConfig.Alliance.BLUE)
+                GlobalConfig.side = GlobalConfig.Side.DEPOT;
+        }
     }
 }
